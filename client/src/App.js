d6c4c8fdc1a45c7fe31da91e2487f7d0e217db7a
Akash Kandasamy <akashram953@gmail.com>
2023-03-25 13:28:36 +0530

    Document Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import './App.css'
-import Nav from './components/nav'
-import Login from './pages/login'
-import Home from './pages/home'
-import AddCard from './pages/addCard'
-// import "atropos/css";
+import "./App.css";
+import Nav from "./components/nav";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import AddCard from "./pages/addCard";
+
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { TransactionsProvider } from './context/TransactionContext'
 
-import React from 'react'
+import React from "react";
+import AddDocument from "./pages/addDocument";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<TransactionsProvider>
-				<Routes>
-					<Route path='/home' element={<Home />} />
-					<Route path='/admin' element={<AddCard />} />
-					<Route path='/' exact element={<Login />} />
-				</Routes>
-			</TransactionsProvider>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <TransactionsProvider>
+        <Nav />
+        <Routes>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/admin" element={<AddCard />}/>
+          <Route path="/addDocument" element={<AddDocument />}/>
+          <Route path="/" exact element={<Login />}/>
+        </Routes>
+      </TransactionsProvider>
+    </BrowserRouter>
+  )
 }
 
 export default App
