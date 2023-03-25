c2121ed609fb1a21d0b5af9efe8121ee7df6e05f
Kushagra Sharma <skushagra2003@gmail.com>
2023-03-25 14:36:49 +0530

    removed unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,8 @@ import "./App.css";
 import Nav from "./components/nav";
 import Login from "./pages/login";
 import Home from "./pages/home";
-import AddCard from "./pages/addCard";
 import AddDocument from './pages/addDocument'
 import Verifier from './pages/verifier'
-import AddData from "./pages/addData";
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { TransactionsProvider } from './context/TransactionContext'
@@ -20,9 +18,7 @@ const App = () => {
 				<Nav />
 				<Routes>
 					<Route path='/home' element={<Home />} />
-					<Route path='/admin' element={<AddCard />} />
 					<Route path='/addDocument' element={<AddDocument />} />
-					<Route path='/addData' element={<AddData />} />
 					<Route path='/verifier' element={<Verifier />} />
 					<Route path='/' exact element={<Login />} />
 				</Routes>
