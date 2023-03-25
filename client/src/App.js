09ac36709299253f18ee06e7f178906425590050
Akash Kandasamy <akashram953@gmail.com>
2023-03-25 22:29:37 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
-import "./App.css";
-import Nav from "./components/nav";
-import Login from "./pages/login";
-import Home from "./pages/home";
+import './App.css'
+import Nav from './components/nav'
+import Login from './pages/login'
+import Home from './pages/home'
 import AddDocument from './pages/addDocument'
 import Verifier from './pages/verifier'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { TransactionsProvider } from './context/TransactionContext'
-import React from "react";
-
+import React from 'react'
 
 const App = () => {
-  return (
+	return (
 		<BrowserRouter>
 			<TransactionsProvider>
-				{window.location.pathname === '/' ? <></> : <Nav />}
 				<Routes>
 					<Route path='/home' element={<Home />} />
 					<Route path='/addDocument' element={<AddDocument />} />
