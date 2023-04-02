388ff212efe1f3f160854c342542eed401160277
DNA5769 <dennisthomas2002@gmail.com>
2023-04-03 02:40:48 +0530

    Removed API calls

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-    baseURL: 'http://localhost:3030'
-});
-
-export const login = (walletHash) => API.post('/user/login', { hash_id: walletHash });
-export const getVerifierHash = (name) => API.post('/verifier/getVerifierHash', { name: name });
-export const getVerifierName = (hash_id) => API.post('/verifier/getVerifierName', { hash_id : hash_id});
